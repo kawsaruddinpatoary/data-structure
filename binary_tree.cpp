03c6891341ce7416c388ea3863a2539a9d89025c
kawsar <kawsaruddin350@gmail.com>
2025-05-29 12:45:17 +0600

    height and count of nodes in a binary tree

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -74,10 +74,30 @@ void levelOrder(Node *root){
     cout << endl;
 }
 
+int height(Node *root){
+    if(root==NULL){
+        return 0;
+    }
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return max(left, right) + 1;
+}
+
+int count(Node *root){
+    if(root==NULL){
+        return 0;
+    }
+    int left = count(root->left);
+    int right = count(root->right);
+
+    return left+right+1;
+}
 
 
 int main(){
     vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
     Node *root = buildTree(preorder);
     levelOrder(root);
+    cout << count(root) << endl;
 }
